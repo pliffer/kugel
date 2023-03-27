8fc7939d80f3e29f2ae89f2036561ddff6114e45
Weslley C <weslleycxs@gmail.com>
2023-03-27 09:16:08 -0300

    Comentários no arquivo start.js

--- boot/start.js
+++ boot/start.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const fs   = require('fs');
 
-process.env.ROOT = path.resolve(__dirname, '../');
+// Define a variável de ambiente ROOT como o caminho absoluto da pasta do projeto, que é onde iniciou o node .
+process.env.ROOT = path.resolve(process.cwd());
 
-const startedAt   = new Date();
+// Armazena a data e hora atuais na variável startedAt para calcular o tempo de inicialização mais tarde
+const startedAt = new Date();
 
 console.log('@starting Iniciando (' + startedAt + ')');
 
+// Verifica se o arquivo .env existe e, caso contrário, criar copiando o arquivo .env.example
 if(!fs.existsSync(process.env.ROOT + '/.env')){
 
     console.log('@starting Criando .env');
@@ -14,20 +17,31 @@ if(!fs.existsSync(process.env.ROOT + '/.env')){
 
 }
 
+// Define uma variável para guardar a função de resolução da Promise
+let triggerStart;
+
+// Exporta uma Promise que será resolvida quando o aplicativo estiver pronto para iniciar
+module.exports = new Promise((resolve) => { triggerStart = resolve });
+
+// Função assíncrona auto-executável para realizar várias verificações e configurações antes de iniciar o aplicativo
 (async () => {
 
     try{
     
+        // Lê arquivos na raiz do projeto
         const rootFiles = fs.readdirSync(process.env.ROOT);
     
+        // Exibe mensagem sobre dependências e recomendações
         console.log('@starting Dependencias e recomendações');
     
+        // Exibe aviso se o projeto não estiver em um repositório Git
         if(!rootFiles.includes('.git')){
     
             console.warn("\n@warning Fora de um projeto git");
     
         }
     
+        // Exibe erro e encerrar o processo se as dependências do projeto não estiverem instaladas
         if(!rootFiles.includes('node_modules') || !rootFiles.includes('package.json')){
     
             console.log("\n@error Instale as dependências: npm install\n");
@@ -35,8 +49,10 @@ if(!fs.existsSync(process.env.ROOT + '/.env')){
     
         }
     
+        // Carrega o arquivo package.json
         let package = require(path.join(process.env.ROOT, './package.json'));
     
+        // Verifica se a propriedade "kugel" está definida no arquivo package.json e exibir erro e encerrar o processo, caso contrário
         if(typeof package.kugel == 'undefined'){
     
             console.log("@fatal O package.json deve possuir a propriedade kugel");
@@ -44,31 +60,40 @@ if(!fs.existsSync(process.env.ROOT + '/.env')){
     
         }
 
+        // Carrega variáveis de ambiente do arquivo .env usando o módulo 'dotenv'
         require('dotenv').config({
             path: process.env.ROOT + '/.env'
         });
 
+        // Define a propriedade 'config' do objeto package.kugel, se não existir
         package.kugel.config ??= {};
 
+        // Se a propriedade 'modules' estiver definida em package.kugel.config, carrega os módulos especificados usando o módulo './module.js'
         if(package.kugel.config.modules){
     
             const module = require('./module.js');
     
+            // Exibir mensagem sobre módulos
             console.log('@starting Modulos');
     
+            // Carregar módulos
             await module.load(package.kugel.modules);
 
         }
 
-        // @future Criar um arquivo que guarda as informações relevantes do processo para debug
+        // Gravar o ID do processo (PID) para fins de depuração
         fs.writeFileSync(path.join(process.env.ROOT, 'lastpid'), process.pid.toString(), 'utf-8')
-
+    
         console.log("@started Iniciado " + (new Date() - startedAt) + "ms");
 
+        // Chama a função de resolução da Promise para iniciar o aplicativo
+        triggerStart();
+
     } catch(err){
-    
+        
+        // Exibe mensagem de erro e encerra o processo
         if(err){
-    
+
             console.error("@error Fatal Error");
             console.error(err);
     
